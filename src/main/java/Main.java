185aecd14d9db393684ecb65261b4137f630b4f5
Rajendra Baviskar <rajendra.baviskar@cerner.com>
2017-05-03 22:12:26 -0500

    Constructor chaining

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by RB034746 on 05/03/2017.
+ */
+public class Main {
+    public static void main(String args [ ]){
+
+        MathEquation testEquation = new MathEquation();
+
+        MathEquation[] mathEquations = new MathEquation[4]; //
+        // Created Array of type MathEquation with size 4
+        mathEquations[0] = new MathEquation(10,20,'a');
+        mathEquations[1] = new MathEquation(10,20,'s');
+        mathEquations[2] = new MathEquation(10,20,'d');
+        mathEquations[3] = new MathEquation(10,20,'m');
+
+        for(MathEquation eachEquation : mathEquations ){
+            eachEquation.execute();
+            System.out.println("Result=" + eachEquation.getResult());
+
+        }
+    }
+}
---
+++ src/main/java/MathEquation.java
@@ -0,0 +1,73 @@
+/**
+ * Created by RB034746 on 05/03/2017.
+ */
+public class MathEquation {
+    private double leftValue;
+    private double rightvalue;
+    private char operation = 'a';
+    private double result;
+
+    public MathEquation(char operation){
+        this.operation = operation;
+    }
+
+    public MathEquation(double leftValue, double rightvalue, char operation){
+        this(operation);
+        this.leftValue = leftValue;
+        this.rightvalue = rightvalue;
+    }
+
+    public void execute(){
+
+        switch (operation){
+            case 'a':
+                result = leftValue + rightvalue;
+                break;
+            case 's':
+                result = leftValue - rightvalue;
+                break;
+            case 'd':
+                result = leftValue / rightvalue;
+                break;
+            case 'm':
+                result = leftValue * rightvalue;
+                break;
+            default:
+                System.out.println("Error: Invalid Operation");
+                result = 0.0d;
+                break;
+        }
+    }
+
+    public double getLeftValue() {
+        return leftValue;
+    }
+
+    public void setLeftValue(double leftValue) {
+        this.leftValue = leftValue;
+    }
+
+    public double getRightvalue() {
+        return rightvalue;
+    }
+
+    public void setRightvalue(double rightvalue) {
+        this.rightvalue = rightvalue;
+    }
+
+    public char getOperation() {
+        return operation;
+    }
+
+    public void setOperation(char operation) {
+        this.operation = operation;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+}
