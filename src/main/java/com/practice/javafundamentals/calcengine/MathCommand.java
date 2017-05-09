5206a8973913be492bb8fd9a9ea536a0c4049340
Rajendra Baviskar <rajendra.baviskar@cerner.com>
2017-05-09 09:44:02 -0500

    META INF

--- src/main/java/com/practice/javafundamentals/calcengine/MathCommand.java
+++ src/main/java/com/practice/javafundamentals/calcengine/MathCommand.java
@@ -3,7 +3,7 @@ package com.practice.javafundamentals.calcengine;
 /**
  * Created by RB034746 on 05/05/2017.
  */
-public enum MathCommand {
+enum MathCommand {
     Add,
     Subtract,
     Multiplay,
